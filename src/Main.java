75ffb7391a527b87254f70b1376da8c79cc5fe44
sven <svenmaster89@gmail.com>
2016-03-29 20:14:28 +0200

    tcp

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,8 @@ public class Main {
     public static void main(String[] args) throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 
-       // if(args.length == 0) Data.ip = UdpDiscover.findIP(Data.discover_message,Data.udpPort); //request message raspi checks for
-       // else Data.ip = args[0];
-
-        Data.ip = "192.168.10.46";
+        if(args.length == 0) Data.ip = UdpDiscover.findIP(Data.discover_message,Data.udpPort); //request message raspi checks for
+        else Data.ip = args[0];
 
         new GuiBuilder("Relais").setVisible(true);
     }
