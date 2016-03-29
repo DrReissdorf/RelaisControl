46cbada11e5991544c74afbf95b321b516f704d4
sven <svenmaster89@gmail.com>
2016-03-30 01:07:09 +0200

    tcp

--- src/Controller.java
+++ src/Controller.java
@@ -4,18 +4,20 @@ import java.net.Socket;
 
 public class Controller {
     private JFrame optionPaneFrame; //fuer popups
+    private Socket socket;
 
     public Controller() {
         optionPaneFrame = new JFrame();
+        socket = connectSocket();
     }
 
-    private Socket connectSocket(String ip, int port) {
+    private Socket connectSocket() {
         try {
-            return new Socket(Data.ip, 18745);
+            return new Socket(Data.ip, Data.tcpPort);
         } catch (IOException e) {
             try {
                 Data.ip = UdpDiscover.findIP(Data.discover_message,Data.udpPort);
-                return new Socket(Data.ip, 18745);
+                return new Socket(Data.ip, Data.tcpPort);
             } catch (IOException ex) {
                 JOptionPane.showMessageDialog(optionPaneFrame, "No answer from server.\nClosing program");
                 System.exit(-1);
@@ -28,9 +30,7 @@ public class Controller {
 
     public boolean sendCommand(String command, String ip, int port) {
         PrintWriter outToServer = null;
-        Socket socket = null;
         try {
-            socket = connectSocket(ip, port);
             outToServer = new PrintWriter(socket.getOutputStream());
             outToServer.println(command);
             outToServer.flush();
@@ -45,11 +45,9 @@ public class Controller {
     public String[] getInfo(String ip, int port, String splitRegex) {
         PrintWriter outToServer = null;
         BufferedReader inFromServer = null;
-        Socket socket = null;
         String[] ret;
 
         try {
-            socket = connectSocket(ip, port);
             outToServer = new PrintWriter(socket.getOutputStream());
             inFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
